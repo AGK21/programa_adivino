060aa0f10866555e47bdae04adb9d612137ef359
AGK21 <73641961+AGK21@users.noreply.github.com>
2020-11-30 12:22:05 +0100

    Update Aciertos.java

--- alfredo_mira_adivino/src/alfredo_mira_adivino/Aciertos.java
+++ alfredo_mira_adivino/src/alfredo_mira_adivino/Aciertos.java
@@ -4,7 +4,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Aciertos {
-
+//Revisado por Antonio Gibbs. Funicona
 	public static void main(String[] args) {
 		
 		int n = 0,cont = 0;
